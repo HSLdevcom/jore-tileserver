2a7d969fa44d6436adcd2103aa308071e5c97e4d
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-09-06 11:48:13 +0300

    Add routes layer

--- index.js
+++ index.js
@@ -14,8 +14,23 @@ const stopsQuery = `
         WHERE ST_Transform(point, 3857) && ST_MakeEnvelope($1, $2, $3, $4, 3857)
     ) AS rows`;
 
+const routesQuery = `
+    SELECT ST_AsMVT('routes', 4096, 'geom', rows)
+    FROM (
+        SELECT
+            route_id,
+            direction,
+            date_begin,
+            date_end,
+            mode,
+            ST_AsMVTGeom(ST_Transform(geom, 3857), ST_MakeEnvelope($1, $2, $3, $4, 3857), 4096, 0, true) AS geom
+        FROM jore.geometry geometry
+        WHERE $5 between date_begin and date_end and ST_Intersects(ST_Transform(geom, 3857), ST_MakeEnvelope($1, $2, $3, $4, 3857))
+    ) AS rows`;
+
 const tileServer = new TileServer({ connectionString: process.env.PG_CONNECTION_STRING });
 
 tileServer.addLayer({ name: "stops", query: stopsQuery });
+tileServer.addLayer({ name: "routes", query: routesQuery });
 
 tileServer.listen(3000);
